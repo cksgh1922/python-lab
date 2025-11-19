135ee966b139636c5d25b57118b00fa367677bef
gould7789 <yukif029@g.yju.ac.kr>
2025-11-19 09:46:47 +0900

    251119

--- russian.py
+++ russian.py
@@ -1,13 +1,13 @@
 import random
 
 bullet = random.randint(0, 2)  # 3칸 중 한 칸에 탄
-print("🔫 러시안룰렛 시작 (스페이스 누르고 엔터)")
+print("러시안룰렛 시작 (스페이스 누르고 엔터)")
 
 while True:
     key = input()  # 스페이스+엔터 누르면 발사
     if key == " ":
         if bullet == 0:
-            print("💥 빵!!!")
+            print("빵!!!")
             break
         else:
             print("삑,,,")
